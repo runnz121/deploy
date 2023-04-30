5a48a77879556b9527bb122beffd0460802a797b
박종빈 <runnz121@gmail.com>
2023-05-01 00:02:19 +0900

    jwt properties

--- src/main/java/com/kuku/deploy/DeployApplication.java
+++ src/main/java/com/kuku/deploy/DeployApplication.java
@@ -2,8 +2,10 @@ package com.kuku.deploy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = { "classpath:jwt.properties" })
 public class DeployApplication {
 
 	public static void main(String[] args) {
