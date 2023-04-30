6fc48bafa93541d20838121ca47045ae8a4b7bbc
박종빈 <runnz121@gmail.com>
2023-04-30 22:28:25 +0900

    create jwt application

--- src/main/java/com/kuku/deploy/HealthCheck.java
+++ src/main/java/com/kuku/deploy/HealthCheck.java
@@ -1,5 +1,6 @@
 package com.kuku.deploy;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/check")
 public class HealthCheck {
 
+    @Value("${jwt.secret-key}")
+    private String secretKey;
+
     @GetMapping
     public String healthCheck() {
-        return "health check";
+        return secretKey;
     }
 }
