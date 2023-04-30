235745d9121953aa704902ba0d161980cc7fb8a8
박종빈 <runnz121@gmail.com>
2023-05-01 01:31:33 +0900

    dd

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,16 +6,16 @@ cd $REPOSITORY
 APP_NAME=deploy
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
-CURRENT_PID=$(pgrep -f $APP_NAME)
-
-if [ -z "$CURRENT_PID" ]
-then
-  echo "> 종료할것 없음."
-else
-  echo "> kill -9 $CURRENT_PID"
-  sudo kill -15 "$CURRENT_PID"
-  sleep 5
-fi
+#CURRENT_PID=$(pgrep -f $APP_NAME)
+#
+#if [ -z $CURRENT_PID ]
+#then
+#  echo "> 종료할것 없음."
+#else
+#  echo "> kill -9 $CURRENT_PID"
+#  sudo kill -9 $CURRENT_PID
+#  sleep 5
+#fi
 
 echo "> $JAR_PATH start"
-sudo nohup java -jar "$JAR_PATH" > /dev/null 2> /dev/null < /dev/null
+sudo nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null
