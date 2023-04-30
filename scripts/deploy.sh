5f06ea468964d18d1f8ec779b6b6e5a4118bc0dc
박종빈 <runnz121@gmail.com>
2023-05-01 00:47:24 +0900

    grant permission

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 REPOSITORY="/home/ubuntu/deploy_test"
+# shellcheck disable=SC2164
 cd $REPOSITORY
 
 APP_NAME="deploy"
 JAR_NAME="$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)"
 JAR_PATH="$REPOSITORY/$JAR_NAME"
-
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
 echo "> $REPOSITORY"
