3298f7872116ff5ee4b6e6ecee942bccc482eb8a
박종빈 <runnz121@gmail.com>
2023-05-01 14:37:05 +0900

    dd

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,12 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-PORT=8080
-
-PID=$(lsof -i:$PORT -t)
-
-if [ -z "$PID" ]; then
-  echo "No process found running on port $PORT"
-else
-  echo "Killing process $PID running on port $PORT"
-  kill -9 $PID
-fi
+sudo killall java
+exit 0
