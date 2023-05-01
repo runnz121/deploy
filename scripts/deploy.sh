fa40506e9e8943fc8f19193cb7a167e2aad02cfd
박종빈 <runnz121@gmail.com>
2023-05-01 13:44:37 +0900

    change path

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ cd $REPOSITORY
 APP_NAME=deploy
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/deploy-0.0.1-SNAPSHOT.jar
-CURRENT_PID=$(pgrep -f $APP_NAME)
+#CURRENT_PID=$(pgrep -f $APP_NAME)
 
 #if [ -z $CURRENT_PID ]
 #then
@@ -18,5 +18,6 @@ CURRENT_PID=$(pgrep -f $APP_NAME)
 #fi
 
 echo "> $JAR_PATH start"
+sudo nohup java -jar /home/ubuntu/deploy_test/build/libs/deploy-0.0.1-SNAPSHOT.jar
 #sudo nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null
-sudo nohup java -jar /home/ubuntu/deploy_test/build/libs/deploy-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null
+#sudo nohup java -jar /home/ubuntu/deploy_test/build/libs/deploy-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null
