de0d20dd8ad0ed82839fc9becf51e72a9e20cb6b
박종빈 <runnz121@gmail.com>
2023-05-01 12:36:50 +0900

    try 2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,17 +5,17 @@ cd $REPOSITORY
 
 APP_NAME=deploy
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
-JAR_PATH="$REPOSITORY/build/libs/deploy-0.0.1-SNAPSHOT.jar"
-#CURRENT_PID=$(pgrep -f $APP_NAME)
-#
-#if [ -z $CURRENT_PID ]
-#then
-#  echo "> 종료할것 없음."
-#else
-#  echo "> kill -9 $CURRENT_PID"
-#  sudo kill -9 $CURRENT_PID
-#  sleep 5
-#fi
+JAR_PATH=$REPOSITORY/build/libs/deploy-0.0.1-SNAPSHOT.jar
+CURRENT_PID=$(pgrep -f $APP_NAME)
+
+if [ -z $CURRENT_PID ]
+then
+  echo "> 종료할것 없음."
+else
+  echo "> kill -9 $CURRENT_PID"
+  sudo kill -9 $CURRENT_PID
+  sleep 5
+fi
 
 echo "> $JAR_PATH start"
 sudo nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null
